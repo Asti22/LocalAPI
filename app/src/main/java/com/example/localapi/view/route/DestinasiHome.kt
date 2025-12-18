5dd65671f128cd0773ffbf41589a68d2219908cc
Asti22 <uun3461@gmail.com>
2025-12-18 15:29:35 +0700

    menambahkan destinasi navigasi app name

---
+++ app/src/main/java/com/example/localapi/view/route/DestinasiHome.kt
@@ -0,0 +1,7 @@
+package com.example.localapi.view.route
+import com.example.localapi.R
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
