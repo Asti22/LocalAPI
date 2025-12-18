f4e954c693a4f47da81a43bbd2642522bad9ade8
Asti22 <uun3461@gmail.com>
2025-12-18 18:58:28 +0700

    perbaika code

--- app/src/main/java/com/example/localapi/MainActivity.kt
+++ app/src/main/java/com/example/localapi/MainActivity.kt
@@ -10,16 +10,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.example.localapi.ui.theme.LocalAPITheme
 import com.example.localapi.view.uicontroller.DataSiswaApp
-class MainActivity : ComponentActivity() {
 
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Mengaktifkan edge-to-edge (status bar & navigation bar transparan)
         enableEdgeToEdge()
 
         setContent {
-            LocalAPITheme {
+            LocalAPITheme {   // ✅ HARUS INI
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
