c8385e58c7032a575b2cfa15173c4766d7a5d461
Asti22 <uun3461@gmail.com>
2025-12-18 09:28:32 +0700

    implement siswa repository with Retrofit service

---
+++ app/src/main/java/com/example/localapi/modeldata/RepositoryDataSiswa.kt
@@ -0,0 +1,25 @@
+package com.example.localapi.modeldata
+
+import com.example.localapi.apiservice.ServiceApiSiswa
+import retrofit2.Response
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void>
+    // suspend fun getSatuSiswa(id: Int): DataSiswa
+    // suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): Response<Void>
+    // suspend fun hapusSatuSiswa(id: Int): Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+) : RepositoryDataSiswa {
+
+    override suspend fun getDataSiswa(): List<DataSiswa> =
+        serviceApiSiswa.getSiswa()
+
+    override suspend fun postDataSiswa(
+        dataSiswa: DataSiswa
+    ): Response<Void> =
+        serviceApiSiswa.postSiswa(dataSiswa)
+}
