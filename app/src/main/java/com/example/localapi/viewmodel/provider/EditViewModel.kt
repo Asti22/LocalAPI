dedeb80f2eb2e42ecfd053209e6e2d91f1459a45
Asti22 <uun3461@gmail.com>
2025-12-25 15:29:01 +0700

    tambah EditViewModel untuk ambil data siswa berdasarkan id

---
+++ app/src/main/java/com/example/localapi/viewmodel/provider/EditViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.localapi.viewmodel.provider
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.localapi.modeldata.UIStateSiswa
+import com.example.localapi.modeldata.toUiStateSiswa
+import com.example.localapi.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
