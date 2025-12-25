9d8604faed30fb3920894f1cb316c5f23dc577ed
Asti22 <uun3461@gmail.com>
2025-12-25 15:46:08 +0700

    tambahkan ViewModelFactory dan penyedia dependency aplikasi

--- app/src/main/java/com/example/localapi/viewmodel/provider/penyediaviewmodel.kt
+++ app/src/main/java/com/example/localapi/viewmodel/provider/penyediaviewmodel.kt
@@ -1,24 +1,30 @@
 package com.example.localapi.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.localapi.repositori.AplikasiDataSiswa
 
 // Fungsi ekstensi yang diperbaiki
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+        )
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
         initializer {
-            HomeViewModel(
+            DetailViewModel(
+                this.createSavedStateHandle(),
                 aplikasiDataSiswa().container.repositoryDataSiswa
             )
         }
         initializer {
-            EntryViewModel(
+            EditViewModel(
+                this.createSavedStateHandle(),
                 aplikasiDataSiswa().container.repositoryDataSiswa
             )
         }
